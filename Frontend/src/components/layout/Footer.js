f7de0ecb07e4a745e77d17556e0edc50ff552327
juan pablo <74376759+juan14p@users.noreply.github.com>
2020-11-12 16:09:15 -0500

    Componente footer
    Se añadio componente footer para cada parte de la app en la parte de usuario ya registrado

---
+++ Frontend/src/components/layout/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div> 
+             <footer>
+                <div className="copyright bg-dark text-white text-center">
+                 <p className="textoFooter">
+                     Todos los derechos reservados ©2020-2021
+                 </p>
+                  <i className="fab fa-instagram fa-2x"></i> &nbsp;&nbsp;
+                  <i className="fab fa-twitter fa-2x"></i> &nbsp;&nbsp;
+                  <i className="fab fa-facebook-square fa-2x"></i> &nbsp;&nbsp;
+                </div>
+             </footer>
+        </div>
+    );
+}
+
+export default Footer;
